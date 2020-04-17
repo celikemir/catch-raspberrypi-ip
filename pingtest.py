65164979663dcc9efa72321ece1fd0a266a3a6f6
celikemir <celikemirhan6@gmail.com>
2020-04-17 19:52:27 +0300

    class type

--- pingtest.py
+++ pingtest.py
@@ -2,36 +2,36 @@ import subprocess
 import os
 import re
 
+class Transform:
 
-def ping(server='camera1.local', count=1, wait_sec=1):
-    """
-    :rtype: dict or None
-    """
-    cmd = "ping -c {} -W {} {}".format(count, wait_sec, server).split(' ')
-    try:
-        output = subprocess.check_output(cmd).decode().strip()
-        lines = output.split("\n")
-        total = lines[-2].split(',')[3].split()[1]
-        loss = lines[-2].split(',')[2].split()[0]
-        timing = lines[-1].split()[3].split('/')
+    prefix = "http://"
+    suffix = ":8081/?action=stream"
 
-        if(loss[0] == "0"):
-            ipaddr = re.findall(r'\(([^)]+)\)', output)[0]
-            ipaddr = ("http://{}:8081/?action=stream\"".format(ipaddr))
 
-        else:
-            print("package is lossed")
+    def __init__(self, server , count = 1, wait_sec = 1):
+        self.server=server
+        """
+        :rtype: dict or None
+        """
+        cmd = "ping -c {} -W {} {}".format(count, wait_sec, self.server).split(' ')
 
-        return {
-            'type': 'rtt',
-            'min': timing[0],
-            'avg': timing[1],
-            'max': timing[2],
-            'mdev': timing[3],
-            'total': total,
-            'loss': loss,
-            'ipaddr': ipaddr
-        }
-    except Exception as e:
-        print(e)
-        return None
+        try:
+            output = subprocess.check_output(cmd).decode().strip()
+            lines = output.split("\n")
+            total = lines[-2].split(',')[3].split()[1]
+            loss = lines[-2].split(',')[2].split()[0]
+            timing = lines[-1].split()[3].split('/')
+
+            if(loss[0] == "0"):
+                self.ipaddr = re.findall(r'\(([^)]+)\)', output)[0]
+                #self.ipaddr = ("http://{}:8081/?action=stream\"".format(self.ipaddr))
+
+        except Exception as e:
+            print(e)
+            return None
+    
+    def mjpegformat(self) -> str:
+        return ( "{prefix}{ip}{suffix}".format(prefix = self.prefix, ip = self.ipaddr ,suffix = self.suffix))
+
+
+#print(Transform("camera1.local").mjpegformat())
